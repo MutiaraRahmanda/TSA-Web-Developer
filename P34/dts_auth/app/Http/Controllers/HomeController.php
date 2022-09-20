17b29eb21c58c375d61e20e43591f17e15e58a77
MutiaraRahmanda <mutiararizkia18@gmail.com>
2022-09-20 19:47:30 +0700

    Change login using username

--- P34/dts_auth/app/Http/Controllers/HomeController.php
+++ P34/dts_auth/app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
@@ -23,6 +26,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        return view('home',['user' => $user]);
     }
 }
